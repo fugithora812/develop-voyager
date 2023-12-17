5364e2342e14f0eda22764f8a4514a84a828e85c
Junpei Kuwata <junpei.kuwata@zenkigen.jp>
2023-12-18 05:17:57 +0900

    update: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
-// @ts-ignore
-import Favicon from '/public/favicon.ico';
 import { Suspense } from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import React from 'react';
+
+// @ts-ignore
+import Favicon from '/public/favicon.ico';
+import './globals.css';
+import { Providers } from './components/Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,8 +18,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
