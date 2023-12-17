d60dbb8b7a4caf90e9512cec07e625bb7fe73ea8
Junpei Kuwata <junpei.kuwata@zenkigen.jp>
2023-12-18 07:09:36 +0900

    fix: ux before display article

--- src/app/atoms/articleCard.tsx
+++ src/app/atoms/articleCard.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { NextPage } from 'next';
 import Link from 'next/link';
@@ -11,26 +12,32 @@ interface Props {
   latest?: boolean;
 }
 
-const ArticleCard: NextPage<Props> = ({ imageUrl, category, title, description, href, latest }: Props) => (
-  <div
-    className={`${latest ? 'w-[49%] min-[1170px]:w-full' : 'w-[49%]'}
-  flex flex-col items-center my-6 space-y-4 md:space-x-4 md:space-y-0 md:flex-row`}
-  >
-    <div className="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-full">
-      <Link href={href} className={`w-inherit block h-full`}>
-        {/* eslint-disable-next-line */}
-        <img alt="blog photo" src={imageUrl} className="object-cover w-full max-h-40" />
-        <div className="w-100 p-4 bg-white dark:bg-gray-800 break-words">
-          <div className="flex items-start mb-3">
-            <div className="badge badge-primary text-md">{category}</div>
-            {latest && <div className="badge badge-secondary ml-1">Latest</div>}
+const ArticleCard: NextPage<Props> = ({ imageUrl, category, title, description, href, latest }: Props) => {
+  const [isClicked, setIsClicked] = React.useState(false);
+
+  return (
+    <div
+      className={`${latest ? 'w-[49%] min-[1170px]:w-full' : 'w-[49%]'}
+  flex flex-col items-center my-6 space-y-4 md:space-x-4 md:space-y-0 md:flex-row relative`}
+    >
+      {isClicked && <span className="loading loading-dots loading-lg absolute inset-x-[50%] inset-y-[60%]" />}
+
+      <div className="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-full ">
+        <Link href={href} className={`w-inherit block h-full`} onClick={() => setIsClicked(true)}>
+          {/* eslint-disable-next-line */}
+          <img alt="blog photo" src={imageUrl} className="object-cover w-full max-h-40" />
+          <div className="w-100 p-4 bg-white dark:bg-gray-800 break-words">
+            <div className="flex items-start mb-3">
+              <div className="badge badge-primary text-md">{category}</div>
+              {latest && <div className="badge badge-secondary ml-1">Latest</div>}
+            </div>
+            <p className="truncate mb-2 text-xl font-medium text-gray-800 dark:text-white">{title}</p>
+            <p className="truncate font-light text-gray-400 dark:text-gray-300 text-md ">{description}</p>
           </div>
-          <p className="truncate mb-2 text-xl font-medium text-gray-800 dark:text-white">{title}</p>
-          <p className="truncate font-light text-gray-400 dark:text-gray-300 text-md ">{description}</p>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ArticleCard;
