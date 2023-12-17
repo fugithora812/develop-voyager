d248972a185a115d655062623f9c379a9f02fbfc
Junpei Kuwata <junpei.kuwata@zenkigen.jp>
2023-12-17 20:24:02 +0900

    update: Suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 // @ts-ignore
 import Favicon from '/public/favicon.ico';
+import { Suspense } from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -16,7 +17,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <Suspense fallback={<span className="loading loading-dots loading-lg" />}>
+        <body className={inter.className}>{children}</body>
+      </Suspense>
     </html>
   );
 }
