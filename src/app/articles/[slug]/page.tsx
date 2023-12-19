5743331b6098472f96fb97d5277890de3a0650b7
Junpei Kuwata <junpei.kuwata@zenkigen.jp>
2023-12-20 05:26:53 +0900

    refactor: article page

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,42 +1,16 @@
 import React from 'react';
 import { HiLink } from 'react-icons/hi';
 import ReactMarkdown from 'react-markdown';
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash';
-import go from 'react-syntax-highlighter/dist/cjs/languages/prism/go';
-import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
-import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json';
-import lua from 'react-syntax-highlighter/dist/cjs/languages/prism/lua';
-import markdown from 'react-syntax-highlighter/dist/cjs/languages/prism/markdown';
-import scss from 'react-syntax-highlighter/dist/cjs/languages/prism/scss';
-import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx';
-import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript';
-import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { type NextPage } from 'next';
 import path from 'path';
-import remarkGfm from 'remark-gfm';
 
+import MarkdownContents from '../../components/MarkdownContents';
 import { getMarkdownData } from '../../lib/markdown';
 
 /* end of import languages for syntax highlighting */
 import Footer from '@/app/atoms/footer';
 import Header from '@/app/atoms/header';
 import fetchArticleMetadata, { type ArticleMetadata } from '@/app/lib/fetchArticleMetadata';
-
-/*
- * setting languages for syntax highlighting.
- * ref: https://biplobsd.me/blogs/view/syntax-highlight-code-in-NextJS-tailwindcss-daisyui.md
- * ref: https://github.com/react-syntax-highlighter/react-syntax-highlighter/blob/master/AVAILABLE_LANGUAGES_PRISM.MD
- */
-SyntaxHighlighter.registerLanguage('tsx', tsx);
-SyntaxHighlighter.registerLanguage('ts', typescript);
-SyntaxHighlighter.registerLanguage('scss', scss);
-SyntaxHighlighter.registerLanguage('bash', bash);
-SyntaxHighlighter.registerLanguage('go', go);
-SyntaxHighlighter.registerLanguage('js', javascript);
-SyntaxHighlighter.registerLanguage('markdown', markdown);
-SyntaxHighlighter.registerLanguage('json', json);
-SyntaxHighlighter.registerLanguage('lua', lua);
 
 const customH2 = ({ ...props }): React.ReactElement => {
   return (
@@ -94,36 +68,10 @@ const ArticlePage: NextPage<Props> = async ({ params }: Props): Promise<React.Re
             {content}
           </ReactMarkdown>
         </ol>
-        <ReactMarkdown
-          remarkPlugins={[remarkGfm]}
-          className="markdown col-start-2"
-          components={{
-            // @ts-expect-error eslint-disable-this-line
-            code({ inline, className, ...props }) {
-              // eslint-disable-next-line
-              const hasLang = /language-(\w+)/.exec(className || '');
-              // eslint-disable-next-line
-              return !inline && hasLang ? (
-                <SyntaxHighlighter
-                  style={oneDark}
-                  language={hasLang[1]}
-                  PreTag="div"
-                  className="mockup-code scrollbar-thin scrollbar-track-base-content/5 scrollbar-thumb-base-content/40 scrollbar-track-rounded-md scrollbar-thumb-rounded m-0"
-                  showLineNumbers
-                  useInlineStyles
-                >
-                  {String(props.children).replace(/\n$/, '')}
-                </SyntaxHighlighter>
-              ) : (
-                <code className={className} {...props} />
-              );
-            },
-          }}
-        >
-          {content}
-        </ReactMarkdown>
+        <div className="col-start-2">
+          <MarkdownContents content={content} />
+        </div>
       </div>
-
       <Footer />
     </div>
   );
