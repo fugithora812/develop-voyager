e8eb3b770bfef279b633ca6a49088da7349974ee
Junpei Kuwata <junpei.kuwata@zenkigen.jp>
2023-12-22 07:04:40 +0900

    update: articles' page

--- src/app/articles/[slug]/ArticleContent.tsx
+++ src/app/articles/[slug]/ArticleContent.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { type NextPage } from 'next';
+import { redirect } from 'next/navigation';
 import path from 'path';
 
 import MarkdownContents from '@/app/atoms/MarkdownContents';
-import NotFound from '@/app/atoms/NotFound';
 import fetchArticleMetadata, { type ArticleMetadata } from '@/app/lib/fetchArticleMetadata';
 import { getMarkdownData } from '@/app/lib/markdown';
 
@@ -18,7 +18,7 @@ const ArticleContent: NextPage<Props> = async ({ slug }: Props): Promise<React.R
     content = await getMarkdownData(pathToContent);
   } catch (error) {
     console.error(error);
-    return <NotFound />;
+    redirect('/404');
   }
 
   const metadata = await fetchArticleMetadata();
@@ -41,8 +41,10 @@ const ArticleContent: NextPage<Props> = async ({ slug }: Props): Promise<React.R
 
       <div className="grid gap-8 md:grid-cols-[20%_60%] lg:gap-12">
         <ol className="p-2 md:p-4 ml-2 md:col-start-1">
-          <h2 className="underline sticky top-16 max-[1010px]:hidden">🖌️ Table of Contents</h2>
-          <h2 className="underline sticky top-16 min-[1010px]:hidden">🖌️ TOC</h2>
+          <h2 className="text-gray-800 dark:text-slate-300 underline sticky top-16 max-[1010px]:hidden">
+            🖌️ Table of Contents
+          </h2>
+          <h2 className="text-gray-800 dark:text-slate-300 underline sticky top-16 min-[1010px]:hidden">🖌️ TOC</h2>
           <MarkdownContents content={content} isToc />
         </ol>
         <div className="col-start-2">
