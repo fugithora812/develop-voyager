63566d0bab0b16416a72f4dc33089151074df7d5
Junpei Kuwata <junpei.kuwata@zenkigen.jp>
2023-12-28 16:29:31 +0900

    update: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,7 +36,7 @@ export const middleware = async (request: NextRequest): Promise<NextResponse | u
     return;
   }
 
-  const res = await fetch(`${request.nextUrl.origin}/api/idToken`, {
+  const res = await fetch(`${process.env.NEXT_BASE_URL}/api/idToken`, {
     method: 'POST',
     body: JSON.stringify({ idToken: cookies().get('next-auth.session-token')?.value }),
   })
