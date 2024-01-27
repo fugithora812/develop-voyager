280b60292e47e89e8623eefafc2a5aed63c84415
Junpei Kuwata <junpei.kuwata@zenkigen.jp>
2024-01-27 18:45:30 +0900

    fix: prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
